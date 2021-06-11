91995abce5ec5d633f372bcd3711278ac90f292a
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-06-11 13:08:46 -0400

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+
+payload = {
+    "id": "776d69dd876bf6ef5c284ee7fc2a1cc2"
+}
+
+t = requests.post("http://127.0.0.1:5000/busca_todas_atualizacoes", json=payload)
+u = requests.post("http://127.0.0.1:5000/busca_ultima_atualizacao", json=payload)
+
+print("-------------Todas as atualizações-------------")
+print(t.text)
+
+print("")
+print("")
+
+print("------------Ultima atualização---------")
+print(u.text)
